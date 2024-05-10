d44d4aa6c80221d78f4794c2b909d8782808975a
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-05-09 22:30:06 -0500

    Se agregan nuevos archivos al repositorio, se trataron ngModule, Componenet, @Input, *ngFor y sus propiedades, *ngIfElse, <ng-template>, [ngClass]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { AppRoutingModule } from './app-routing.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
-
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
